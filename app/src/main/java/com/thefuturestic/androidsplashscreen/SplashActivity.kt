167d41271b7963e6a0ab18d2dfbc2333fa61a7df
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2020-06-17 19:50:00 +0500

    initial commit

---
+++ app/src/main/java/com/thefuturestic/androidsplashscreen/SplashActivity.kt
@@ -0,0 +1,47 @@
+package com.thefuturestic.androidsplashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+        val background = object : Thread() {
+            override fun run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    Thread.sleep(1000)
+
+                    // After 5 seconds redirect to another intent
+                    val i = Intent(baseContext, MainActivity::class.java)
+                    startActivity(i)
+                    finish()
+
+                } catch (e: Exception) {
+
+                }
+
+            }
+        }
+
+        // start thread
+        background.start()
+
+    }
+
+    override fun onDestroy() {
+
+        super.onDestroy()
+
+    }
+}
